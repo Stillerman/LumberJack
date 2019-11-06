7e03e51f5d2754c9eaca219d321065a67f28581c
Jason Stillerman <jts@ip0af54c84.int.uvm.edu>
2019-11-06 12:54:41 -0500

    update and delete for userevents on the api

--- API/routes/userEvents.js
+++ API/routes/userEvents.js
@@ -1,11 +1,15 @@
 import { Router } from 'express'
 const router = Router()
-import { create, getById, getAll} from '../app/api/controllers/userEvents'
+import { create, getById, getAll, deleteById, updateById} from '../app/api/controllers/userEvents'
 
 
 router.post('/', create) // with userEvent: {...} in the body
+
 router.get('/:userEventId', getById)
 router.get('/', getAll)
+
+router.delete('/:userEventId', deleteById)
+router.put('/:userEventId', updateById) // with userEvent: {...} in the body
 
 
 export default router
